7a7ca651d89bf9dc57af82bb3867522a171a544c
Marijan <marijannovak123@gmail.com>
2018-04-04 23:10:58 +0200

    small update

--- app/src/main/java/com/marijannovak/autismhelper/modules/login/adapters/LoginSignupPagerAdapter.kt
+++ app/src/main/java/com/marijannovak/autismhelper/modules/login/adapters/LoginSignupPagerAdapter.kt
@@ -3,17 +3,27 @@ package com.marijannovak.autismhelper.modules.login.adapters
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import com.marijannovak.autismhelper.modules.login.LoginFragment
+import com.marijannovak.autismhelper.modules.login.SignupFragment
 
-class LoginRegisterPagerAdapter(private val fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+class LoginSignupPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
 
-
+    private var currentFragment : Fragment? = null
 
     override fun getItem(position: Int): Fragment {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        currentFragment = when(position) {
+            0 -> LoginFragment.newInstance()
+            else -> SignupFragment.newInstance()
+        }
+
+        return currentFragment as Fragment
     }
 
-    override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getCount(): Int = 2
 
+    override fun getPageTitle(position: Int): CharSequence? =
+            when(position) {
+                0 -> "Login"
+                else -> "Sign Up"
+            }
 }
