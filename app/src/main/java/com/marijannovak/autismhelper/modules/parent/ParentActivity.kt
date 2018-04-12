8b16c0afc5189c25c8019f22001e355844533184
Marijan <marijannovak123@gmail.com>
2018-04-12 10:54:31 +0200

    parent drawer setup

--- app/src/main/java/com/marijannovak/autismhelper/modules/parent/ParentActivity.kt
+++ app/src/main/java/com/marijannovak/autismhelper/modules/parent/ParentActivity.kt
@@ -1,18 +1,53 @@
 package com.marijannovak.autismhelper.modules.parent
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.view.MenuItem
 import com.marijannovak.autismhelper.R
 import com.marijannovak.autismhelper.common.base.ViewModelActivity
 import com.marijannovak.autismhelper.common.enums.Enums
 import com.marijannovak.autismhelper.data.repo.DataRepository
+import com.marijannovak.autismhelper.modules.parent.fragments.DashboardFragment
 import com.marijannovak.autismhelper.modules.parent.mvvm.ParentRepository
 import com.marijannovak.autismhelper.modules.parent.mvvm.ParentViewModel
+import kotlinx.android.synthetic.main.activity_parent.*
 
 class ParentActivity : ViewModelActivity<ParentViewModel>() {
+
+    private var currentFragment: Fragment? = DashboardFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parent)
+
+        setupDrawer()
+    }
+
+    private fun setupDrawer() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+
+        loadCurrentFragmnet()
+
+        navView.setNavigationItemSelectedListener {item -> handleNavViewClick(item)  }
+    }
+
+    private fun loadCurrentFragmnet() {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.llContainer, currentFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private fun handleNavViewClick(item: MenuItem): Boolean {
+        currentFragment = null
+        when(item.itemId) {
+
+        }
+
+        return true
     }
 
     override fun createViewModel() = ParentViewModel(ParentRepository(), DataRepository())
@@ -28,5 +63,22 @@ class ParentActivity : ViewModelActivity<ParentViewModel>() {
 
     override fun showError(throwable: Throwable) {
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers()
+        }
     }
 }
---
+++ app/src/main/java/com/marijannovak/autismhelper/modules/parent/fragments/DashboardFragment.kt
@@ -0,0 +1,24 @@
+package com.marijannovak.autismhelper.modules.parent.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.marijannovak.autismhelper.R
+
+
+class DashboardFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+
+
+
+}
